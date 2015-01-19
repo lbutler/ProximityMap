ee016e68ad68ebb323d72acf50cdb0d7e87722fb
Luke Butler <lukepbutler@gmail.com>
2015-01-19 23:19:39 +1100

    hubblesite.org public domain images
    Using some cool looking hubble images for my demo

--- demo.js
+++ demo.js
@@ -4,17 +4,28 @@
 
 	proximityApp.waitingZone = new PROXIMITY.Zone("0", "Waiting Zone", document.getElementById('waiting-zone') );
 
-
 	//Webservice returns known beacons and zones
-	var knownBeacons = [{
-		"uuid": 1,
-		"name": "Peter",
-		"imgUrl": "http://imgsrc.hubblesite.org/hu/db/images/hs-1994-02-c-thumb.jpg"
-	},{
-		"uuid": 2,
-		"name": "Luke",
-		"imgUrl": "http://imgsrc.hubblesite.org/hu/db/images/hs-1994-02-c-thumb.jpg"
-	}];
+	// Images from HubbleSite http://hubblesite.org
+	// http://hubblesite.org/about_us/copyright.php
+	var knownBeacons = [
+		{"uuid": 1, "name": "Black Eye Galaxy", "imgUrl": "img/BlackEyeGalaxy.jpg"},
+		{"uuid": 2, "name": "Cats Eye Nebula", "imgUrl": "img/CatsEyeNebula.jpg"},
+		{"uuid": 3, "name": "Cluster M13", "imgUrl": "img/ClusterM13.jpg"},
+		{"uuid": 4, "name": "Galaxy M81", "imgUrl": "img/GalaxyM81.jpg"},
+		{"uuid": 5, "name": "Galaxy M106", "imgUrl": "img/GalaxyM106.jpg"},
+		{"uuid": 6, "name": "Jupiter", "imgUrl": "img/Jupiter.jpg"},
+		{"uuid": 7, "name": "Galaxy M16", "imgUrl": "img/M16.jpg"},
+		{"uuid": 8, "name": "Mars", "imgUrl": "img/Mars.jpg"},
+		{"uuid": 9, "name": "NGC1309", "imgUrl": "img/NGC1309.jpg"},
+		{"uuid":10, "name": "Pluto", "imgUrl": "img/PlutoAndMoons.jpg"},
+		{"uuid":11, "name": "NGC2174", "imgUrl": "img/NGC2174.jpg"},
+		{"uuid":12, "name": "NGC3370", "imgUrl": "img/NGC3370.jpg"},
+		{"uuid":13, "name": "Ring", "imgUrl": "img/Ring.jpg"},
+		{"uuid":14, "name": "Saturn", "imgUrl": "img/Saturn.jpg"},
+		{"uuid":15, "name": "Super Nova", "imgUrl": "img/SuperNova.jpg"},
+		{"uuid":16, "name": "Uranus", "imgUrl": "img/Uranus.jpg"},
+		{"uuid":17, "name": "Turbulent Star", "imgUrl": "img/TurbulentStar.jpg"},
+		{"uuid":18, "name": "Crab Nebula", "imgUrl": "img/CrabNebula.jpg"}];
 
 	knownBeacons.forEach( function(beacon) {
 		proximityApp.addBeacon( new PROXIMITY.Beacon(beacon.uuid, beacon.name, beacon.imgUrl) );
