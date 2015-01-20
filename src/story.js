6aa808c7dee253c76ead6e35d63f182e4d6cb4d5
Luke Butler <lukepbutler@gmail.com>
2015-01-20 21:22:24 +1100

    Limit the length of story displayed
    Pretty simple atm

--- src/story.js
+++ src/story.js
@@ -6,13 +6,19 @@ PROXIMITY.Story = (function() {
 	var Story = function Story(dom) {
 
 		this.dom = dom;
+		this.count = 0;
+		this.max = 20;
 
 	};
 
 	Story.prototype.updateStory = function(beacon, zone, action) {
 
 		$( this.dom ).prepend( "<li><b>" + beacon.name + "</b> has " + action + " <b>" + zone.name + "</b> <span data-livestamp='" + Date.now()/1000 + "'></span>.</li>");
-
+		this.count++;
+		if(this.count > this.max) {
+			$( this.dom ).find("li").slice(-10).remove();
+			this.count -= 10;
+		}
 	};
 
 
